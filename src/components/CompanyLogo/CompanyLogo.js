6d3908b0700661a009c084bef78beee079a2be67
CodeNinja <54727654+BerkAngay-terminal@users.noreply.github.com>
2021-05-10 16:47:48 +0300

    CompanyLogo component created.

---
+++ src/components/CompanyLogo/CompanyLogo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function CompanyLogo() {
+    return (
+        <div className="flex-shrink-0 px-4 flex items-center">
+            <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                alt="Workflow"
+            />
+        </div>
+    )
+}
